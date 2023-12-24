049dd3fe78cc2029cd0b7238477d40f75279037a
Artyom <48983679+AlphaWoll@users.noreply.github.com>
2023-12-25 02:44:18 +0300

    app.js

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
-// JavaScript Document
+function onEntry(entry){
+    entry.forEach(change => {
+        if(change.isIntersecting){
+            change.target.classList.add('show');
+        } else{
+        change.target.classList.remove('show');
+        }
+    });
+}
+
+let options = {threshold: [0.5] };
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.anim');
+for (let elm of elements){
+    observer.observe(elm);
+}
 //открытие на полный экран
 const slider = document.querySelector('.slider');
 const slides = Array.from(slider.querySelectorAll('img'));
